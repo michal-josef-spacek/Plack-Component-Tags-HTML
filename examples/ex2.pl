fef904cbb5d9f2bb9b0d989ae21cf2856f5c7a80
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-02-27 19:54:30 +0100

    Fix example output in doc

--- examples/ex2.pl
+++ examples/ex2.pl
@@ -28,5 +28,17 @@ my $app = App->new(
 my $runner = Plack::Runner->new;
 $runner->run($app);
 
+# HTTP::Server::PSGI: Accepting connections at http://0:5000/
+
+# Output by HEAD to http://localhost:5000/:
+# 200 OK
+# Date: Sun, 27 Feb 2022 18:52:59 GMT
+# Server: HTTP::Server::PSGI
+# Content-Length: 11
+# Content-Type: text/html; charset=utf-8
+# Client-Date: Sun, 27 Feb 2022 18:52:59 GMT
+# Client-Peer: 127.0.0.1:5000
+# Client-Response-Num: 1
+
 # Output by GET to http://localhost:5000/:
-# TODO
+# Hello world
