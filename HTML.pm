defc425259a1ba063b0bd349139a33e7e9416502
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2022-02-28 12:38:33 +0100

    Prepare for 0.03 version

--- HTML.pm
+++ HTML.pm
@@ -12,7 +12,7 @@ use Tags::HTML::Page::Begin;
 use Tags::HTML::Page::End;
 use Tags::Output::Raw;
 
-our $VERSION = 0.02;
+our $VERSION = 0.03;
 
 sub call {
 	my ($self, $env) = @_;
@@ -438,6 +438,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.02
+0.03
 
 =cut
